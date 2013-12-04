eac0605d276206858e8cd56136e8c2acce0b76e9
eddiefisher <eddifisher@bk.ru>
2013-12-04 22:37:23 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TalksterImage::Application.config.secret_key_base = '303de5ccfb79cad14c7963029addad6d49d2efe3d6b25f90c3fdb40b824c2acffdb93baa082bba60e19deee2c79f2c0eeb5d50f1041bca82ffa17cde097ae535'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TalksterImage::Application.config.session_store :cookie_store, key: '_talkster_image_session'
